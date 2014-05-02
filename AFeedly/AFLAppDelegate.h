82174e0b28f22520d8f527998824ce720d2411cb
Alkim Gozen <alkimake@gmail.com>
2014-05-02 22:27:02 +0300

    Initial Commit

---
+++ AFeedly/AFLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AFLAppDelegate.h
+//  AFeedly
+//
+//  Created by Alkim Gozen on 02/05/14.
+//  Copyright (c) 2014 Alkimake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFeedly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AFeedly
+//
+//  Created by Alkim Gozen on 02/05/14.
+//  Copyright (c) 2014 Alkimake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AFLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AFLAppDelegate class]));
+    }
+}
